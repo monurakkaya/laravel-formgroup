beffb97c2045a29b0bfb7a0bf992208b5b067d38
Monur <monurakkaya@gmail.com>
2018-08-17 09:15:23 +0300

    added multiple option to file type inputs

--- src/resources/views/components/formgroup/fields/file.blade.php
+++ src/resources/views/components/formgroup/fields/file.blade.php
@@ -1,12 +1,15 @@
 <input
         type="file"
-        name="{{ $name }}"
+        name="{{ $name }}{{ ($multiple ?? false) ? '[]' : '' }}"
         class="form-control"
         @if (($required ?? false))
             required
         @endif
         @if (($max ?? false))
             maxlength="{{ $max }}"
+        @endif
+        @if ($multiple ?? false)
+            multiple
         @endif
         value="{{ old($name, request($name)) }}"
 >
